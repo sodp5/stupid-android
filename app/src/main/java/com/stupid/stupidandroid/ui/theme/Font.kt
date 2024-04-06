23425d405453d1d84f57e0c40636a59ac5801e44
eshc.joon <eshc.joon@gmail.com>
2024-04-06 11:07:00 +0900

    add font

---
+++ app/src/main/java/com/stupid/stupidandroid/ui/theme/Font.kt
@@ -0,0 +1,15 @@
+package com.stupid.stupidandroid.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.stupid.stupidandroid.R
+
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+)
