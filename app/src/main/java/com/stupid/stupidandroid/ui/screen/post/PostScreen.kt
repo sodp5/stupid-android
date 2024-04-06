c23ffd8ba17efdbd807c5c64d67b67ba23b1cc3e
dean <sodp5@naver.com>
2024-04-06 12:43:02 +0900

    Post Progress 추가

--- app/src/main/java/com/stupid/stupidandroid/ui/screen/post/PostScreen.kt
+++ app/src/main/java/com/stupid/stupidandroid/ui/screen/post/PostScreen.kt
@@ -2,16 +2,20 @@ package com.stupid.stupidandroid.ui.screen.post
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun PostScreen(
-    modifier : Modifier = Modifier,
+    modifier: Modifier = Modifier,
     viewModel: PostViewModel = hiltViewModel()
 ) {
     PostScreen(
@@ -23,12 +27,20 @@ fun PostScreen(
 @Composable
 fun PostScreen(
     modifier: Modifier = Modifier
-){
+) {
     Column(
         modifier = modifier
             .background(Color.White),
     ) {
         PostTitleBar()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        PostProgress(
+            modifier = Modifier
+                .padding(horizontal = 72.dp),
+            currentStep = PostProgressStep.Second,
+        )
     }
 }
 
